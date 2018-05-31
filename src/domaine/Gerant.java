e070066a7305d35a69f585b6afa104765b40d84a
BadrA <azeri.badreddine@gmail.com>
2018-05-31 14:26:53 +0200

    création de la classe gerant

---
+++ src/domaine/Gerant.java
@@ -0,0 +1,38 @@
+package domaine;
+
+public class Gerant extends Personne {
+	
+	private Agence agence;
+
+	/**
+	 * constructeur avec paramétre
+	 * @param id
+	 * @param nom
+	 * @param prenom
+	 * @param agence
+	 */
+	
+	public Gerant(int id, String nom, String prenom, Agence agence) {
+		super(id, nom, prenom);
+		this.agence = agence;
+	}
+
+	/**
+	 * getter
+	 * @return the agence
+	 */
+	public Agence getAgence() {
+		return agence;
+	}
+
+	/**
+	 * setter
+	 * @param agence the agence to set
+	 */
+	public void setAgence(Agence agence) {
+		this.agence = agence;
+	}
+	
+	
+
+}
