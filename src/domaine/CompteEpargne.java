b86a54cc6d0e4ae89add939b0d2dbcb04bc288d5
ArnaudV <vallette.arnaud@gmail.com>
2018-05-31 14:45:32 +0200

    Creation classe Compte Epargne

---
+++ src/domaine/CompteEpargne.java
@@ -0,0 +1,64 @@
+package domaine;
+
+/**
+ * Classe CompteEpargne qui est Compte ayant un taux de rémunération.
+ * @author Adminl
+ *
+ */
+public class CompteEpargne extends Compte {
+	
+	private double taux;
+
+	//CONSTRUCTEUR
+	
+	/**
+	 * Constructeur par défaut
+	 */
+	public CompteEpargne() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+	/**
+	 * Cosntructeur tous attributs.
+	 * @param taux
+	 */
+	public CompteEpargne(double taux) {
+		super();
+		this.taux = taux;
+	}
+
+	//GETTER AND SETTER
+
+	/**
+	 * @return the taux
+	 */
+	public double getTaux() {
+		return taux;
+	}
+
+
+
+	/**
+	 * @param taux the taux to set
+	 */
+	public void setTaux(double taux) {
+		this.taux = taux;
+	}
+
+
+	//TO STRING
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "CompteEpargne [id=" + super.getId() + ", solde=" + super.getSolde() + ", dateOuverture=" + super.getDateOuverture() + "taux=" + taux + "]";
+	}
+	
+	
+	
+	
+}
