5ce45582af1660108e1d66488e3b995f0db62798
BadrA <azeri.badreddine@gmail.com>
2018-05-31 13:50:35 +0200

    création de la classe Personne

---
+++ src/domaine/Personne.java
@@ -0,0 +1,92 @@
+/**
+ * 
+ */
+package domaine;
+
+/**
+ * @author Adminl
+ *
+ */
+public class Personne {
+	private int id;
+	private String nom;
+	private String prenom;
+	
+	/**
+	 * consructeur avec paramètres
+	 * @param id
+	 * @param nom
+	 * @param prenom
+	 */
+	public Personne(int id, String nom, String prenom) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+	}
+
+	/**
+	 * consturcteur sans paramètres
+	 */
+	public Personne() {
+		super();
+	}
+
+	/**
+	 * getter et setter
+	 */
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+
+	/**
+	 * @param nom the nom to set
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	/**
+	 * @return the prenom
+	 */
+	public String getPrenom() {
+		return prenom;
+	}
+
+	/**
+	 * @param prenom the prenom to set
+	 */
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	/**
+	 * methode toString
+	 */
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Personne [id=" + id + ", nom=" + nom + ", prenom=" + prenom + "]";
+	}
+
+
+}
